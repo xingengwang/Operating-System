cea228ec08dc1bd2cd5fcdf595f88fa63efaebe9
xiw031@USASK.CA <xiw031@USASK.CA@58e8e8f5-899b-491c-b118-a2a382736acb>
2016-09-15 01:08:47 +0000

    add commonWin.h, modify partB.bash commonwin.c partA.c

--- a1/commonWin.c
+++ a1/commonWin.c
@@ -1,5 +1,32 @@
 #include <time.h>
 #include <stio.h>
 
-void S(int N);
 
+
+int Square(int N)
+{
+  printf("hello word!");
+  return 0;
+}
+
+int getSysTime(int *returnTime)
+{
+  clock_t clockTime;s
+  int currTimeInms;
+  double milliseconds;
+  
+  clockTime = clock();
+  milliseconds = ((double)clockTime / CLOCKS_PER_SEC)*1000;
+  currTimeInms = (int) milliseconds;
+  
+  if(returnTime != NULL)
+  {
+    *returnTime = currTimeInms;
+  }
+  else
+  {
+    return -1;
+  }
+  
+
+}
---
+++ a1/commonWin.h
@@ -0,0 +1,8 @@
+#ifndef COMMONWIN_H_
+#define COMMONWIN_H_
+
+int Square(int N);
+
+int getSysTime(int *returnTime);
+
+#endif
