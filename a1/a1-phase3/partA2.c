f6069647181bcc089faa2586a691b5e0fd328c7b
xiw031@USASK.CA <xiw031@USASK.CA@58e8e8f5-899b-491c-b118-a2a382736acb>
2016-10-01 21:57:18 +0000

    UPDATE A2

--- a1/a1-phase3/partA2.c
+++ a1/a1-phase3/partA2.c
@@ -1,13 +1,41 @@
 //Xingeng Wang, xiw031, 11144515
 //Yuchen Lin  , yul761, 11138672
 #include "commonWin.h"
-#include <time.h>
+
 #include <stdio.h>
+#include <standards.h>
+#include <os.h>
 
+PID pid_parent;
+PID pidb;
 
-
-int mainp()
+PROCESS a(int sem)
 {
-  printf("This is a dummy pthread call!");
-  return 0;
+    int times = TIMES;
+
+    P(sem);
+    printf("1 done P\n");
+    V(sem);
+    printf("1 done V\n");
+
+    printf("a is sending a message to b \n");
+    Send(pidb, NULL, 0);
+
+
+    printf("a received a reply from b \n");
+
+    while(times--)
+	{
+        Sleep(4);
+	printf("fasttimo\n");
+        }
+}
+
+void mainp()
+{
+   long sem;
+
+    setbuf(stdout, 0);
+    sem = NewSem(0);
+    pid_parent = Create( (void(*)()) a, 16000, "a", (void *) sem, NORM, USR );
 }
