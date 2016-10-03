008578ddcfcc74782f3af443666939d282c88f43
xiw031 <xiw031@USASK.CA>
2016-10-03 03:30:07 +0000

    ubc

--- a1/a1-phase3/partA3.c
+++ a1/a1-phase3/partA3.c
@@ -1,13 +1,50 @@
-//Xingeng Wang, xiw031, 11144515
-//Yuchen Lin  , yul761, 11138672
-#include "commonWin.h"
-#include <time.h>
+/*Xingeng Wang, xiw031, 11144515
+Yuchen Lin  , yul761, 11138672*/
+#include "common.h"
 #include <stdio.h>
+#include <standards.h>
+#include <os.h>
+#include<kernel.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 
+volatile int THREAD,DEADLINE,SIZE;
 
-int main()
+void *child_thread(void *SIZE)
 {
-  printf("This is a dummy posix thread call!\n");
-  return 0;
+  int i;
+  for(i=0; i<= SIZE ;i=i+1)
+  {
+     Square(i);
+  }
+  printf("child pid %d called square() %d times in %d ms\n",, counter[&child],5);
+}
+
+ void *parent_thread(void *THREAD)
+ {
+	int i;
+	pthread_t child;
+  for(i=0; i< THREAD ;i=i+1)
+  {
+    pthread_create( &child, NULL, child_thread, (void *)SIZE );
+  }
+  printf("Child: creating thread\n");  
+    pthread_exit((void*) t);
+ }
+
+
+int main (int argc, char *argv[])
+{
+     
+  THREAD= atoi(argv[1]);
+  DEADLINE= atoi(argv[2]);
+  SIZE= atoi(argv[3]);
+  
+  pthread_t parent;
+  int mother;
+  mother = pthread_create( &parent, NULL, parent_thread, (void *)THREAD );
+  
+  printf("Main: creating thread\n");    
+  pthread_exit(NULL);
 }
